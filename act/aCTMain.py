9944b1a836977d770d2ab34405ae899c4d47582c
David Cameron <david.cameron@cern.ch>
2013-08-08 17:47:17 +0200

    change main to use aCTProcessManager

--- act/aCTMain.py
+++ act/aCTMain.py
@@ -1,100 +1,39 @@
 import os
-import subprocess
-import time
-import sys
 import aCTConfig
 import aCTProxy
 import aCTLogger
-import sys
-from threading import Thread
 import aCTSignal
 import aCTUtils
-import pickle
-import re
-
-
-class aCTProcess:
-    """
-    Process control class to spawn aCTSubmitter and aCTDownloader. 
-    """
-    def __init__(self,name):
-        self.name=name
-        self.child=None
-        self.fdout=open(name+".log","a")
-        self.fderr=open(name+".err","a")
-    def start(self):
-        self.child=subprocess.Popen([sys.executable,self.name+".py"],stdout=self.fdout,stderr=self.fderr)
-    def check(self):
-        return self.child.poll()
-    def restart(self):
-        if self.check() != None:
-            self.start()
+import aCTProcessManager
         
 class aCTMain:
-
     """
-    Main class to run aCT. Three major functions: init, run, finish
+    Main class to run aCT.
     """
 
     def __init__(self):
 
         # xml config file
-        self.conf=aCTConfig.aCTConfig()
+        self.conf = aCTConfig.aCTConfig()
         # create log dirs
         try:
-           os.mkdir(self.conf.get(["tmp","dir"]))
+            os.mkdir(self.conf.get(["tmp","dir"]))
         except:
-           pass
+            pass
         try:
-           os.mkdir(str(self.conf.get(["tmp","dir"]))+"/log")
+            os.mkdir(str(self.conf.get(["tmp","dir"]))+"/log")
         except:
-           pass
+            pass
         # logger
-        self.logger=aCTLogger.aCTLogger("main")
-        self.log=self.logger()
+        self.logger = aCTLogger.aCTLogger("main")
+        self.log = self.logger()
         self.log.info("start")
-
-        # dictionary with process descriptions
-        self.procs={}
-
-        self.procs['aCTAutopilot']={}
-        self.procs['aCTAutopilot']['program']=aCTProcess('aCTAutopilot')
-        self.procs['aCTSubmitter']={}
-        self.procs['aCTSubmitter']['program']=aCTProcess('aCTSubmitter')
-        self.procs['aCTResolver']={}
-        self.procs['aCTResolver']['program']=aCTProcess('aCTResolver')
-        self.procs['aCTDownloader']={}
-        self.procs['aCTDownloader']['program']=aCTProcess('aCTDownloader')
-        self.procs['aCTCleaner']={}
-        self.procs['aCTCleaner']['program']=aCTProcess('aCTCleaner')
+        
+        # process manager
+        self.procmanager = aCTProcessManager.aCTProcessManager(self.log, self.conf)
 
         # proxy extender
-        self.proxy=aCTProxy.aCTProxy(Interval=3600)
-        
-
-    def checkProcs(self):
-        # check for crashed processes and respawn
-        for p in self.procs.keys():
-            pr=self.procs[p]['program']
-            rc=pr.check()
-            #print rc
-            if rc == None :
-               pass
-               self.log.debug("process %s is running" % p )
-            else:
-               self.log.info("restarting process %s" % p )
-               pr.restart()
-               pass
-
-            
-    def init(self):
-        """
-        Init the ACT. Start the subprocesses aCTDownloader, aCTSubmitter
-        """
-        self.log.info("init")
-        for p in self.procs.keys():
-            pr=self.procs[p]['program']
-            pr.start()
+        self.proxy = aCTProxy.aCTProxy(Interval=3600)
 
 
     def run(self):
@@ -102,21 +41,19 @@ class aCTMain:
         Main loop
         """
         try:
-           self.log.info("Running")
-           self.checkProcs()
-
-           while 1:
-              # try to reparse config file if it was modified. 
-              self.conf.parse()
-              self.proxy.renew()
-              # check processes
-              self.checkProcs()
-              # 
-              aCTUtils.sleep(10)
+            self.log.info("Running")
+            while 1:
+                self.proxy.renew()
+                # check running processes are ok
+                self.procmanager.checkRunning()
+                # start and stop new processes as necessary
+                self.procmanager.checkClusters()
+                # sleep
+                aCTUtils.sleep(10)
 
         except aCTSignal.ExceptInterrupt,x:
-           print x
-           return
+            print x
+            return
             
 
     def finish(self):
@@ -125,10 +62,8 @@ class aCTMain:
         """
         self.log.info("Cleanup")
 
-
         
 if __name__ == '__main__':
-    am=aCTMain()
-    am.init()
+    am = aCTMain()
     am.run()
     am.finish()
