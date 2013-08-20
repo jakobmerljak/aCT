07d45cb3f316f3d117208e8e2802baeb6748c114
Jon Kerr Nilsen <j.k.nilsen@fys.uio.no>
2013-08-20 10:31:34 +0200

    added forgotten single quote to timestamp

--- act/aCTDB.py
+++ act/aCTDB.py
@@ -21,10 +21,11 @@ class aCTDB(aCTDBMS):
     
     
     def getTimeStamp(self, seconds=None):
+        # todo: move to aCTDBMS and use internal dbms mechanisms for converting
         if seconds:
-            return datetime.datetime.fromtimestamp(seconds).strftime("%Y-%m-%d %H:%M:%S")
+            return "'"+datetime.datetime.fromtimestamp(seconds).strftime("%Y-%m-%d %H:%M:%S")+"'"
         else:
-            return datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+            return "'"+datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")+"'"
     
     
     def createTables(self):
