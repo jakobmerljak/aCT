2501b22e2ffbb5d614b9194cccc380bfd39ba935
David Cameron <david.cameron@cern.ch>
2014-02-11 10:59:15 +0100

    fix insertion into pandajobs

--- act/aCTDBPanda.py
+++ act/aCTDBPanda.py
@@ -62,7 +62,7 @@ class aCTDBPanda(aCTDB):
         desc['created']=self.getTimeStamp()
         desc['pandaid']=pandaid
         desc['pandajob']=pandajob
-        s="insert into pandajobs " + ",".join(['%s=%%s' % (k) for k in desc.keys()])
+        s="insert into pandajobs (" + ",".join([k for k in desc.keys()]) + ") values (" + ",".join(['%s' for k in desc.keys()]) + ")"
         c=self.getCursor()
         c.execute(s,desc.values())
         self.conn.commit()
