e88e27ad397ee6bc2b8097d0453018047a829a8a
Jon Kerr Nilsen <j.k.nilsen@fys.uio.no>
2014-02-21 10:02:16 +0100

    increase sleep time a bit, should probably be configurable

--- act/aCTATLASProcess.py
+++ act/aCTATLASProcess.py
@@ -56,7 +56,7 @@ class aCTATLASProcess:
                 # do class-specific things
                 self.process()
                 # sleep
-                aCTUtils.sleep(1)
+                aCTUtils.sleep(10)
                 # restart periodically in case of hangs
                 #ip=int(self.conf.get(['periodicrestart', self.name.lower()]))
                 #if time.time()-self.starttime > ip and ip != 0 :
