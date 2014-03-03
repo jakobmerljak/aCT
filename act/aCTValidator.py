97a5957fe7a64bada8bcce2314e7305d73d46cd0
David Cameron <david.cameron@cern.ch>
2014-03-03 14:24:13 +0100

    make method names protocol-agnostic, implement output file checking

--- act/aCTValidator.py
+++ act/aCTValidator.py
@@ -1,7 +1,9 @@
 from aCTATLASProcess import aCTATLASProcess
+from aCTProxy import aCTProxy
 import os
 import tarfile
 import re
+import arc
 from xml.dom import minidom
 
 class aCTValidator(aCTATLASProcess):
@@ -11,8 +13,30 @@ class aCTValidator(aCTATLASProcess):
 
     def __init__(self):
         aCTATLASProcess.__init__(self)
+        
+        # Use production role proxy for checking and removing files
+        # Get DN from configured proxy file
+        uc = arc.UserConfig()
+        uc.ProxyPath(str(self.conf.get(['voms', 'proxypath'])))
+        cred = arc.Credential(uc)
+        dn = cred.GetIdentityName()
 
-    def extractSRMFilesFromMetadata(self, arcjobid):
+        actp = aCTProxy(self.log)
+        # Beware hard-coded production role
+        proxyfile = actp.path(dn, '/atlas/Role=production')
+        if not proxyfile:
+            raise Exception('Could not find proxy with production role in proxy table')
+            
+        self.uc = arc.UserConfig()
+        self.uc.ProxyPath(str(proxyfile))
+        
+        # Possible file status
+        self.ok = 0
+        self.retry = 1
+        self.failed = 2
+        
+
+    def extractOutputFilesFromMetadata(self, arcjobid):
         self.log.debug(arcjobid)
         aj = self.dbarc.getArcJobInfo(arcjobid, columns=['JobID'])
         jobid=aj['JobID']
@@ -52,7 +76,7 @@ class aCTValidator(aCTATLASProcess):
                 outp = False
 
             if outp:
-                checksum = "ad:"+ adler32
+                checksum = "adler32:"+ adler32
                 if not surls.has_key(se):
                     surls[se]= []
                 surls[se] += [{"surl":surl, "fsize":size, "checksum":checksum, "arcjobid":arcjobid}]
@@ -60,13 +84,70 @@ class aCTValidator(aCTATLASProcess):
         return surls
             
 
-    def checkSRMFiles(self, surls):
+    def checkOutputFiles(self, surls):
         '''
-        Check if SURLs are working. Returns a dict of arcjobid:success
+        Check if SURLs are working. Returns a dict of arcjobid:file status
+        Do bulk arc.DataPoint.Stat() with max 100 files per request. The list
+        of surls passed here all belong to the same SE.
         '''
-        pass
+        result = {}
+        datapointlist = arc.DataPointList()
+        surllist = []
+        count = 0
+        bulklimit = 100
+        for surl in surls:
+            count += 1
+            dp = arc.datapoint_from_url(surl['surl'], self.uc)
+            datapointlist.append(dp)
+            surllist.append(surl)
+            
+            if count % bulklimit != 0 and count != len(surls):
+                continue
+            
+            # do bulk call
+            (files, status) = dp.Stat(datapointlist)
+            if not status:
+                # If call fails it is generally a server or connection problem
+                # and in most cases should be retryable
+                if status.Retryable():
+                    self.log.warning("Failed to query files on %s, will retry later: %s" % (dp.GetURL().Host(), str(status)))
+                    result.update(dict((k['arcjobid'], self.retry) for k in surllist))
+                else:
+                    self.log.error("Failed to query files on %s: %s" % (dp.GetURL().Host(), str(status)))
+                    result.update(dict((k['arcjobid'], self.failed) for k in surllist))
+            
+            else:
+                # files is a list of FileInfo objects. If file is not found or has
+                # another error in the listing FileInfo object will be invalid
+                for i in range(len(datapointlist)):
+                    if not files[i]:
+                        self.log.warning("Failed to find info on %s" % datapointlist[i].GetURL().str())
+                        result[surllist[i]['arcjobid']] = self.failed
+                    else:
+                        # compare metadata
+                        self.log.debug("File %s: expected size %d, checksum %s, actual size %d, checksum %s" %
+                                       (datapointlist[i].GetURL().str(), surllist[i]['fsize'],
+                                        surllist[i]['checksum'], files[i].GetSize(), files[i].GetCheckSum()))
+                        if surllist[i]['fsize'] != files[i].GetSize():
+                            self.log.warning("File %s: size on storage (%d) differs from expected size (%d)" %
+                                             (datapointlist[i].GetURL().str(), files[i].GetSize(), surllist[i]['fsize']))
+                            result[surllist[i]['arcjobid']] = self.failed
+                        elif surllist[i]['checksum'] != files[i].GetCheckSum():
+                            self.log.warning("File %s: checksum on storage (%s) differs from expected checksum (%s)" %
+                                             (datapointlist[i].GetURL().str(), files[i].GetCheckSum(), surllist[i]['checksum']))
+                            result[surllist[i]['arcjobid']] = self.failed
+                        else:
+                            self.log.info("File %s validated" % datapointlist[i].GetURL().str())
+                            result[surllist[i]['arcjobid']] = self.ok
+                            
+            # Clear lists and go to next round
+            datapointlist = arc.DataPointList()
+            surllist = []
+        
+        return result
+    
 
-    def removeSRMFiles(self, surls):
+    def removeOutputFiles(self, surls):
         '''
         Remove SURLs.
         '''
@@ -75,7 +156,7 @@ class aCTValidator(aCTATLASProcess):
     def validateFinishedJobs(self):
         '''
         Check for jobs with actpandastate tovalidate and pandastatus running
-        Check if the srms in metadata.xml are valid.
+        Check if the output files in metadata.xml are valid.
         If yes, move to actpandastatus to finished, if not, move pandastatus
         and actpandastatus to failed. 
         '''
@@ -89,11 +170,11 @@ class aCTValidator(aCTATLASProcess):
             # nothing to do
             return
         
-        # pull out srm info from metadata.xml into dict, order by SE
+        # pull out output file info from metadata.xml into dict, order by SE
 
         surls = {}
         for job in jobstoupdate:
-            surls.update(self.extractSRMFilesFromMetadata(job["arcjobid"]))
+            surls.update(self.extractOutputFilesFromMetadata(job["arcjobid"]))
 
         if not surls:
             # nothing to validate
@@ -101,9 +182,9 @@ class aCTValidator(aCTATLASProcess):
 
         # check if surls valid, update pandastate accordingly
         for se in surls:
-            checkedsurls = self.checkSRMFiles(surls[se])
-            for id,passed in checkedsurls.items():
-                if passed:
+            checkedsurls = self.checkOutputFiles(surls[se])
+            for id, result in checkedsurls.items():
+                if result == self.ok:
                     select = "arcjobid='"+str(id)+"'"
                     desc = {}
                     desc["pandastatus"] = "finished"
@@ -112,12 +193,16 @@ class aCTValidator(aCTATLASProcess):
                     # set arcjobs state toclean
                     desc = {"arcstate":"toclean", "tarcstate": self.dbarc.getTimeStamp()}
                     self.dbarc.updateArcJobLazy(id, desc)
-                else:
+                elif result == self.failed:
                     # todo: output file failed, should clean up output
                     select = "arcjobid='"+str(id)+"'"
                     desc = {}
                     desc["actpandastatus"] = "tobekilled"
                     self.dbpanda.updateJobsLazy(select, desc)
+                else:
+                    # Retry next time
+                    pass
+                
         self.dbpanda.Commit()
         self.dbarc.Commit()            
                 
@@ -125,7 +210,7 @@ class aCTValidator(aCTATLASProcess):
     def cleanFailedJobs(self):
         '''
         Check for jobs with actpandastate tovalidate and pandastatus failed
-        Delete the srm files in metadata.xml.
+        Delete the output files in metadata.xml.
         Move actpandastatus to failed. 
         '''
         # get all jobs with pandastatus running and actpandastatus tovalidate
@@ -137,18 +222,18 @@ class aCTValidator(aCTATLASProcess):
             # nothing to do
             return
 
-        # pull out srm info from metadata.xml into dict, order by SE
+        # pull out output file info from metadata.xml into dict, order by SE
 
         surls = {}
         for job in jobstoupdate:
-            surls.update(self.extractSRMFilesFromMetadata(job["arcjobid"]))
+            surls.update(self.extractOutputFilesFromMetadata(job["arcjobid"]))
 
         if not surls:
             # nothing to clean
             return
         
         for se in surls:
-            self.removeSRMFiles(surls[se])
+            self.removeOutputFiles(surls[se])
         
         for job in jobstoupdate:
             select = "arcjobid='"+str(job["arcjobid"])+"'"
@@ -166,7 +251,7 @@ class aCTValidator(aCTATLASProcess):
     def cleanResubmittingJobs(self):
         '''
         Check for jobs with actpandastate toresubmit and pandastatus starting
-        Delete the srm files in metadata.xml.
+        Delete the output files in metadata.xml.
         Move actpandastatus to starting. 
         '''
         # get all jobs with pandastatus running and actpandastatus tovalidate
@@ -178,18 +263,18 @@ class aCTValidator(aCTATLASProcess):
             # nothing to do
             return
 
-        # pull out srm info from metadata.xml into dict, order by SE
+        # pull out output file info from metadata.xml into dict, order by SE
 
         surls = {}
         for job in jobstoupdate:
-            surls.update(self.extractSRMFilesFromMetadata(job["arcjobid"]))
+            surls.update(self.extractOutputFilesFromMetadata(job["arcjobid"]))
 
         if not surls:
             # nothing to clean
             return
         
         for se in surls:
-            self.removeSRMFiles(surls[se])
+            self.removeOutputFiles(surls[se])
 
         for job in jobstoupdate:
             select = "arcjobid='"+str(job["arcjobid"])+"'"
