073ae03f0684c4299ed24858b325dbe578fc19c4
David Cameron <david.cameron@cern.ch>
2014-05-11 20:53:03 +0200

    safer mkdir for proxydir

--- act/aCTDBArc.py
+++ act/aCTDBArc.py
@@ -12,7 +12,7 @@ class aCTDBArc(aCTDB):
         conf = aCTConfig.aCTConfigARC()
         self.proxydir=conf.get(["voms","proxystoredir"])
         if not os.path.isdir(self.proxydir):
-            os.mkdir(self.proxydir)
+            os.makedirs(self.proxydir)
                 
         # mapping from Job class attribute types to column types
         self.jobattrmap = {int: 'integer',
