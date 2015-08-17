2c7b1908aa6ec77f87757739eeb163e9cd1e5308
ATLAS aCT 1 <atlact1@voatlas403.cern.ch>
2015-08-17 14:25:56 +0200

    handle errors downloading from AGIS

--- src/act/atlas/aCTAGISFetcher.py
+++ src/act/atlas/aCTAGISFetcher.py
@@ -1,5 +1,6 @@
 # Handler for fetching site info from AGIS. Fetches data and stores it in json file.
 
+from datetime import datetime, timedelta
 import os
 import time
 import urllib2
@@ -15,12 +16,28 @@ class aCTAGISFetcher(aCTATLASProcess):
         self.pilotmanager = self.conf.get(['agis','pilotmanager'])
 
     def fetchFromAgis(self):
-        response = urllib2.urlopen(self.srv)
+        try:
+            response = urllib2.urlopen(self.srv)
+        except urllib2.URLError as e:
+            self.log.warning("Failed to contact AGIS: %s" % str(e))
+            # Check if the cached data is getting old, if so raise a critical error
+            filename = self.conf.get(['agis','jsonfilename'])
+            try:
+                mtime = os.stat(filename).st_mtime
+                if datetime.fromtimestamp(mtime) < datetime.now() - timedelta(hours=1):
+                    self.log.critical("AGIS info has not been updated since more than 1 hour ago")
+            except:
+                # file may not have been created yet
+                pass
+            return ''
+
         urldata = response.read()
         self.log.debug("Fetched %s" % self.srv)
         return urldata
     
     def storeToFile(self, agisjson):
+        if not agisjson:
+            return
         filename = self.conf.get(['agis','jsonfilename'])
         tmpfile=filename+'_'
         try:
@@ -38,19 +55,14 @@ class aCTAGISFetcher(aCTATLASProcess):
         """
         Main loop
         """        
-        try:
-            self.log.info("Running")
-            # todo: check if agis.json exists and return if too new
-            # fetch data from AGIS
-            agisjson = self.fetchFromAgis()
-            # store data to file
-            self.storeToFile(agisjson)
-            # temporary hack to avoid too much agis fetching
-            time.sleep(600)
-        except aCTSignal.ExceptInterrupt,x:
-            self.log.error(x)
-            return
-
+        self.log.info("Running")
+        # todo: check if agis.json exists and return if too new
+        # fetch data from AGIS
+        agisjson = self.fetchFromAgis()
+        # store data to file
+        self.storeToFile(agisjson)
+        # temporary hack to avoid too much agis fetching
+        time.sleep(600)
         
 if __name__ == '__main__':
     aaf=aCTAGISFetcher()
