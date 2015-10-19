23ab1747f81c0bbf2542e8c352bb79f3bebcbd7c
ATLAS aCT 1 <act-dev@cern.ch>
2015-10-19 16:34:51 +0200

    allow setting arbitrary fields in aCTPandaJob

--- src/act/atlas/aCTPandaJob.py
+++ src/act/atlas/aCTPandaJob.py
@@ -13,45 +13,14 @@ class aCTPandaJob:
     
     def __init__(self, jobinfo={}, filehandle=None, filename=''):
         '''
-        Make a new aCTPandaJob. jobid and state are mandatory. jobinfo is a
+        Make a new aCTPandaJob. jobId and state are mandatory. jobinfo is a
         dictonary which can contain any of this class' attributes. If filehandle
         is given pickled info is loaded from the file object. If filename is
         given pickled info is read from a file.
         '''
         self.jobId = None
         self.state = None
-        self.token = None
-        self.transExitCode = None
-        self.pilotErrorCode = None
-        self.pilotErrorDiag = None
-        self.timestamp = None
         self.timeout = 60
-        self.xml = ''
-        self.node = None
-        self.workdir = None
-        self.cpuConsumptionTime = None
-        self.cpuConsumptionUnit = None
-        self.remainingSpace = None
-        self.schedulerID = None
-        self.pilotID = None
-        self.siteName = None
-        self.messageLevel = None
-        self.pilotLog = ''
-        self.metaData = ''
-        self.cpuConversionFactor = None
-        self.exeErrorCode = None
-        self.exeErrorDiag = None
-        self.pilotTiming = None
-        self.computingElement = None
-        self.startTime = None
-        self.endTime = None
-        self.nEvents = None
-        self.nInputFiles = None
-        self.batchID = None
-        self.attemptNr = None
-        self.jobMetrics = None
-        self.stdout = ''
-        self.jobSubStatus=None
         
         if jobinfo:
             self.setAttributes(jobinfo)
@@ -62,19 +31,24 @@ class aCTPandaJob:
                 jobinfo = pickle.load(f)
                 self.setAttributes(jobinfo)
     
+    def __setattr__(self, name, value):
+        '''
+        Override to allow setting arbitrary key value pairs
+        '''
+        self.__dict__[name] = value
+
     def setAttributes(self, jobinfo):
         '''
         Set attributes in the jobinfo dictionary
         '''
         for key, value in jobinfo.iteritems():
-            if key in self.__dict__:
-                self.__dict__[key] = value     
+            self.__dict__[key] = value     
 
     def dictionary(self):
         '''
         Return a dictionary of all the attributes with set values
         '''
-        return dict((k,v) for k,v in self.__dict__.iteritems() if v)
+        return self.__dict__
     
     def writeToFile(self, filename):
         '''
@@ -90,6 +64,7 @@ class aCTPandaJob:
             
 if __name__ == '__main__':
     pj = aCTPandaJob(jobinfo={'jobId': 1234, 'state': 'running', 'batchID': 1234})  
+    pj.newfield = 'blabla'
     print pj.__dict__  
     print pj.dictionary()
     pj.writeToFile('/tmp/x/pickle')
