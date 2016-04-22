99aed0dcae3caf08aa974f2a1794a3b4b4344732
David Cameron <david.cameron@cern.ch>
2016-04-22 15:54:57 +0200

    refactored daemon part, now restart command is available

--- src/act/common/aCTMain.py
+++ src/act/common/aCTMain.py
@@ -40,7 +40,8 @@ class aCTMain:
             self.log.critical(traceback.format_exc())
             self.log.critical("*** Process exiting ***")
             raise e
-        
+
+
     def checkARC(self):
         """
         Check ARC can be used and is correct version
@@ -55,9 +56,65 @@ class aCTMain:
             print 'Error: Found ARC version %s. aCT requires 4.0.0 or higher' % arc.ARC_VERSION
             sys.exit(1)
 
-    def daemon(self, operation):
+
+    def start(self):
         """
-        Start or stop process
+        Start daemon
+        """
+        pidfile = self.conf.get(['actlocation', 'pidfile'])
+        try:
+            with open(pidfile) as f:
+                pid = f.read()
+                if pid:
+                    print "aCT already running (pid %s)" % pid
+                    sys.exit(1)
+        except IOError:
+            pass
+        
+            
+        print 'Starting aCT... '
+        # do double fork
+        try:
+            pid = os.fork()
+            if pid > 0:
+                # exit first parent
+                sys.exit(0)
+        except OSError, e:
+            print "fork #1 failed: %d (%s)" % (e.errno, e.strerror)
+            sys.exit(1)
+    
+        # decouple from parent environment
+        os.setsid()
+        os.umask(0)
+    
+        # do second fork
+        try:
+            pid = os.fork()
+            if pid > 0:
+                # exit from second parent
+                sys.exit(0)
+        except OSError, e:
+            print "fork #2 failed: %d (%s)" % (e.errno, e.strerror)
+            sys.exit(1)
+    
+        # redirect standard file descriptors
+        sys.stdout.flush()
+        sys.stderr.flush()
+        si = open('/dev/null', 'r')
+        so = open('/dev/null', 'a+')
+        se = open('/dev/null', 'a+')
+        os.dup2(si.fileno(), sys.stdin.fileno())
+        os.dup2(so.fileno(), sys.stdout.fileno())
+        os.dup2(se.fileno(), sys.stderr.fileno())
+
+        # write pidfile
+        with open(pidfile,'w+') as f:
+            f.write(str(os.getpid()))
+
+
+    def stop(self):
+        """
+        Stop daemon
         """
         pidfile = self.conf.get(['actlocation', 'pidfile'])
         pid = None
@@ -66,73 +123,44 @@ class aCTMain:
                 pid = f.read()
         except IOError:
             pass
-        
-        if operation == 'start':
-            if pid:
-                print "aCT already running (pid %s)" % pid
-                sys.exit(1)
-                
-            print 'Starting aCT... '
-            # do double fork
-            try:
-                pid = os.fork()
-                if pid > 0:
-                    # exit first parent
-                    sys.exit(0)
-            except OSError, e:
-                print "fork #1 failed: %d (%s)" % (e.errno, e.strerror)
-                sys.exit(1)
-        
-            # decouple from parent environment
-            os.setsid()
-            os.umask(0)
-        
-            # do second fork
-            try:
-                pid = os.fork()
-                if pid > 0:
-                    # exit from second parent
-                    sys.exit(0)
-            except OSError, e:
-                print "fork #2 failed: %d (%s)" % (e.errno, e.strerror)
-                sys.exit(1)
-        
-            # redirect standard file descriptors
-            sys.stdout.flush()
-            sys.stderr.flush()
-            si = open('/dev/null', 'r')
-            so = open('/dev/null', 'a+')
-            se = open('/dev/null', 'a+')
-            os.dup2(si.fileno(), sys.stdin.fileno())
-            os.dup2(so.fileno(), sys.stdout.fileno())
-            os.dup2(se.fileno(), sys.stderr.fileno())
 
-            # write pidfile
-            with open(pidfile,'w+') as f:
-                f.write(str(os.getpid()))
-                
+        if not pid:
+            print 'aCT already stopped'
+            return 1
+
+        try:
+            os.kill(int(pid), signal.SIGTERM)
+        except OSError: # already stopped
+            pass
+        
+        os.remove(pidfile)
+        print 'Stopping aCT... ',
+        sys.stdout.flush()
+        while True:
+            try:
+                aCTUtils.sleep(1)
+                os.kill(int(pid), 0)
+            except OSError as err:
+                if err.errno == errno.ESRCH:
+                    break
+        print 'stopped'
+        return 0
+
+
+    def daemon(self, operation):
+        """
+        Start or stop process
+        """
+     
+        if operation == 'start':
+            self.start()
         elif operation == 'stop':
-            if not pid:
-                print 'aCT already stopped'
-            else:
-                try:
-                    os.kill(int(pid), signal.SIGTERM)
-                except OSError: # already stopped
-                    pass
-                os.remove(pidfile)
-                print 'Stopping aCT... ',
-                sys.stdout.flush()
-                while True:
-                    try:
-                        aCTUtils.sleep(1)
-                        os.kill(int(pid), 0)
-                    except OSError as err:
-                        if err.errno == errno.ESRCH:
-                            break
-                print 'stopped'
-            sys.exit(0)
+            sys.exit(self.stop())
+        elif operation == 'restart':
+            self.stop()
+            self.start()
         else:
-            print 'Usage: python aCTMain.py [start|stop]'
+            print 'Usage: python aCTMain.py [start|stop|restart]'
             sys.exit(1)
                 
                 
