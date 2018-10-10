3cdc5497c7c59c38344c40dad7f7b71617b10c61
davidgcameron <d.g.cameron@gmail.com>
2018-10-10 15:42:54 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(name='aCT',
+      version='0.1',
+      description='ATLAS Control Tower',
+      url='http://github.com/ATLASControlTower/aCT',
+      python_requires='>=2.7',
+      author='aCT team',
+      author_email='act-dev@cern.ch',
+      license='MIT',
+      packages=find_packages(),
+      install_requires=[
+          'mysql-connector == 2.1.*',  # connection to MySQL database
+          'htcondor',                 # bindings to use HTCondor to submit jobs
+          'pylint',                   # for travis automatic tests
+          'requests'                  # for APF mon calls
+          ]
+)
+ 
