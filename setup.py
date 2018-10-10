184a903b18f00bb4a6bf2a759c5ca3ee13e401a3
davidgcameron <d.g.cameron@gmail.com>
2018-10-10 16:03:51 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(name='aCT',
       version='0.1',
@@ -8,7 +8,7 @@ setup(name='aCT',
       author='aCT team',
       author_email='act-dev@cern.ch',
       license='MIT',
-      packages=['src/act'],
+      packages=find_packages('src'),
       install_requires=[
           'mysql-connector == 2.1.*',  # connection to MySQL database
           'htcondor',                 # bindings to use HTCondor to submit jobs
