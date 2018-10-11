90baf1cff2f938dd9581e152922891bb8e3375c4
David Cameron <d.g.cameron@gmail.com>
2018-10-11 11:23:28 +0200

    fix package find

--- setup.py
+++ setup.py
@@ -8,7 +8,8 @@ setup(name='aCT',
       author='aCT team',
       author_email='act-dev@cern.ch',
       license='MIT',
-      packages=find_packages('src/act'),
+      package_dir = {'': 'src'},
+      packages=find_packages('src'),
       install_requires=[
           'mysql-connector == 2.1.*',  # connection to MySQL database
           'htcondor',                 # bindings to use HTCondor to submit jobs
