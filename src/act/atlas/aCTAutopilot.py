450f4e30834264f0a54ca17dad046313fdba6e9c
ATLAS ACT 1 <atlact1@cern.ch>
2019-02-21 13:35:26 +0100

    clean pickle and eventranes

--- src/act/atlas/aCTAutopilot.py
+++ src/act/atlas/aCTAutopilot.py
@@ -290,11 +290,23 @@ class aCTAutopilot(aCTATLASProcess):
         if not jobs:
             return
         
-        # Clean inputfiles
+        # Clean inputfiles, pickle and eventranges
         for j in jobs:
             pandaid=j['pandaid']
             pandainputdir = os.path.join(self.conf.get(["tmp", "dir"]), 'inputfiles', str(pandaid))
+            picklefile = os.path.join(self.conf.get(["tmp", "dir"]), 'pickle', str(pandaid+".pickle"))
+            eventrangesfile = os.path.join(self.conf.get(["tmp", "dir"]), 'eventranges', str(pandaid+".json"))
             shutil.rmtree(pandainputdir, ignore_errors=True)
+            # remove pickle
+            try:
+                os.unlink(picklefile)
+            except:
+                pass
+            # remove eventrangesfile
+            try:
+                os.unlink(eventrangesfile)
+            except:
+                pass
 
         self.log.info("Updating panda for %d finished jobs (%s)" % (len(jobs), ','.join([str(j['pandaid']) for j in jobs]))) 
         
