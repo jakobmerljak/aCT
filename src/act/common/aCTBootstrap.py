21ab95feb78ab613581583c61fb47527dae3f396
David Cameron <d.g.cameron@gmail.com>
2019-10-23 14:04:15 +0200

    put back atlas processes and tables

--- src/act/common/aCTBootstrap.py
+++ src/act/common/aCTBootstrap.py
@@ -16,29 +16,30 @@ def bootstrap_conf():
         print 'Error processing ARC config file: %s' % str(e)
         sys.exit(1)
 
-    #try:
-    #    atlasconf = aCTConfigATLAS()
-    #except Exception as e:
-    #    print 'Error processing ATLAS config file: %s' % str(e)
-    #    sys.exit(1)
+    try:
+        atlasconf = aCTConfigATLAS()
+    except Exception as e:
+        print 'Error processing ATLAS config file: %s' % str(e)
+        sys.exit(1)
 
 
 def bootstrap_db():
     '''Set up the DB tables'''
+    # TODO: setup only what is needed based on config
     logger = aCTLogger('aCTBootstrap')
     log = logger()
     dbarc = aCTDBArc(log)
     dbclient = ClientDB(log)
-    #dbcondor = aCTDBCondor(log)
-    #dbpanda = aCTDBPanda(log)
+    dbcondor = aCTDBCondor(log)
+    dbpanda = aCTDBPanda(log)
     if not dbarc.createTables():
         print('Error creating arc tables, see aCTBootstrap.log for details')
     if not dbclient.createTables():
         print('Error creating client tables, see aCTBootstrap.log for details')
-    #if not dbcondor.createTables():
-    #    print('Error creating condor tables, see aCTBootstrap.log for details')
-    #if not dbpanda.createTables():
-    #    print('Error creating panda tables, see aCTBootstrap.log for details')
+    if not dbcondor.createTables():
+        print('Error creating condor tables, see aCTBootstrap.log for details')
+    if not dbpanda.createTables():
+        print('Error creating panda tables, see aCTBootstrap.log for details')
 
 
 def main():
