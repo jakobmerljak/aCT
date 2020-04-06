a007f0bc0c20490727d6ae422e05a280745e32c3
David Cameron <d.g.cameron@gmail.com>
2020-04-06 22:21:38 +0200

    generate jobs from config and fill xrsl from template values

--- src/act/ldmx/aCTLDMX2Arc.py
+++ src/act/ldmx/aCTLDMX2Arc.py
@@ -33,19 +33,29 @@ class aCTLDMX2Arc(aCTLDMXProcess):
         # Get CE endpoints, filter by status and maxjobs
         onlinesites = [s for s, i in self.sites.items() if i['status'] == 'online' and i['maxjobs'] > 0]
         sitejobs = {s: self.dbldmx.getNJobs(f"ldmxstatus in ('submitted', 'queueing', 'running') AND sitename='{s}'") for s in onlinesites}
-        self.log.debug(', '.join([f'{s}: submitted jobs {n}, maxjobs {self.sites[s]["maxjobs"]}' for (s, n) in sitejobs.items()]))
+        if sitejobs:
+            self.log.debug(', '.join([f'{s}: submitted jobs {n}, maxjobs {self.sites[s]["maxjobs"]}' for (s, n) in sitejobs.items()]))
         onlinesites = [s for s in onlinesites if sitejobs[s] < self.sites[s]['maxjobs']]
         onlineendpoints = [ep for ep, s in self.endpoints.items() if s in onlinesites]
-        self.log.debug(f'Available CEs: {", ".join([f"{self.endpoints[ce]}: {ce}" for ce in onlineendpoints])}')
 
         if not onlineendpoints:
+            self.log.debug('No available CEs')
             return
 
+        self.log.debug(f'Available CEs: {", ".join([f"{self.endpoints[ce]}: {ce}" for ce in onlineendpoints])}')
+
         # Submit waiting jobs
-        waitingjobs = self.dbldmx.getJobs("ldmxstatus='waiting' limit 10")
+        waitingjobs = self.dbldmx.getJobs("ldmxstatus='waiting' order by modified limit 10")
         for job in waitingjobs:
 
-            xrsl = self.createXRSL(job['description'])
+            xrsl = self.createXRSL(job['description'], job['template'])
+            if not xrsl:
+                self.log.warning(f'Could not create xrsl for {job["id"]}')
+                # Set back to new to put at the back of the queue
+                self.dbldmx.updateJobLazy(job['id'], {'ldmxstatus': 'new'})
+                continue
+
+            self.log.info(f'Inserting job {job["id"]} with xrsl {xrsl}')
             arcid = self.dbarc.insertArcJobDescription(xrsl,
                                                        proxyid=job['proxyid'],
                                                        clusterlist=','.join(onlineendpoints),
@@ -54,7 +64,6 @@ class aCTLDMX2Arc(aCTLDMXProcess):
 
             desc = {'ldmxstatus': 'submitted', 'arcjobid': arcid['LAST_INSERT_ID()']}
             self.dbldmx.updateJobLazy(job['id'], desc)
-            self.log.info(f'Inserted job {job["id"]} with xrsl {xrsl}')
 
             nsubmitted += 1
             if nsubmitted >= int(maxsubmitted):
@@ -65,18 +74,31 @@ class aCTLDMX2Arc(aCTLDMXProcess):
             self.dbldmx.Commit()
 
 
-    def createXRSL(self, descriptionfile):
+    def createXRSL(self, descriptionfile, templatefile):
+
         xrsl = {}
+
+        # Parse some requirements from descriptionfile
+        with open(descriptionfile) as f:
+            config = {l.split('=')[0]: l.split('=')[1].strip() for l in f}
+            xrsl['memory'] = int(config.get('JobMemory', 2)) * 1000
+            xrsl['walltime'] = int(config.get('JobWallTime', 240))
+            xrsl['cputime'] = xrsl['walltime']
+            if 'RunTimeEnvironment' in config: # TODO multiple RTE!
+                xrsl['runtimeenvironment'] = f"APPS/{config.get('RunTimeEnvironment')}"
+
         wrapper = self.conf.get(['executable', 'wrapper'])
         xrsl['executable'] = os.path.basename(wrapper)
-        xrsl['inputfiles'] = f'({os.path.basename(wrapper)} {wrapper})'
+        xrsl['inputfiles'] = f'({os.path.basename(wrapper)} {wrapper}) \
+                               (ldmxjob.config {descriptionfile}) \
+                               (ldmx.mac.template {templatefile}) \
+                               (ldmx-prod-helper.py {self.conf.get(["executable", "ruciohelper"])})'
         xrsl['stdout'] = 'stdout'
         xrsl['gmlog'] = 'gmlog'
         xrsl['join'] = 'yes'
         xrsl['rerun'] = '2'
-        xrsl['walltime'] = 30
-        xrsl['cputime'] = 30
         xrsl['outputfiles'] = '("rucio.metadata" "")'
+        xrsl['jobName'] = '"LDMX Prod Simulation"'
 
         return '&' + '\n'.join(f'({k} = {v})' for k,v in xrsl.items())
 
--- src/act/ldmx/aCTLDMXGetJobs.py
+++ src/act/ldmx/aCTLDMXGetJobs.py
@@ -13,13 +13,27 @@ class aCTLDMXGetJobs(aCTLDMXProcess):
     def __init__(self):
         aCTLDMXProcess.__init__(self)
 
+    def generateJobs(self, config):
+
+        randomseed1 = int(config['RandomSeed1SequenceStart'])
+        randomseed2 = int(config['RandomSeed2SequenceStart'])
+        njobs = int(config['NumberofJobs'])
+        self.log.info(f'Creating {njobs} jobs')
+        for n in range(njobs):
+            config['RandomSeed1'] = randomseed1
+            config['RandomSeed2'] = randomseed2
+            yield config
+            randomseed1 += 1
+            randomseed2 += 1
+
     def getNewJobs(self):
         '''
-        Read new job files in buffer dir
+        Read new job files in buffer dir and create necessary job descriptions
         '''
 
         bufferdir = self.conf.get(['jobs', 'bufferdir'])
-        jobs = os.listdir(bufferdir)
+        configsdir = os.path.join(bufferdir, 'configs')
+        jobs = [os.path.join(configsdir, j) for j in os.listdir(configsdir) if os.path.isfile(os.path.join(configsdir, j))]
         now = time.time()
         try:
             proxyid = self.dbarc.getProxiesInfo("attribute=''", ['id'], expect_one=True)['id']
@@ -27,21 +41,45 @@ class aCTLDMXGetJobs(aCTLDMXProcess):
             self.log.error('No proxies found in DB')
             return
 
-        for job in jobs:
-            jobfile = os.path.join(bufferdir, job)
+        for jobfile in jobs:
+
             # Avoid partially written files by delaying the read
             if now - os.path.getmtime(jobfile) < 5:
                 self.log.debug(f'Job {jobfile} is too new')
                 continue
 
             self.log.info(f'Picked up job at {jobfile}')
-            newjobfile = os.path.join(self.tmpdir, job)
-            with tempfile.NamedTemporaryFile(prefix=f'{newjobfile}.', delete=False) as f:
-                newjobfile = f.name
-                shutil.move(jobfile, newjobfile)
 
-            self.dbldmx.insertJob(newjobfile, proxyid)
-            self.log.info(f'Inserted job at {newjobfile} into DB')
+            with open(jobfile) as f:
+                try:
+                    config = {l.split('=')[0]: l.split('=')[1].strip() for l in f}
+                except Exception as e:
+                    self.log.error(f'Failed to parse job config file {jobfile}: probably badly-formed lines or missing "Template": {e}')
+                    os.remove(jobfile)
+                    continue
+
+                try:
+                    templatefile = os.path.join(bufferdir, 'templates', config['Template'])
+                    with open(templatefile) as tf:
+                        pass
+                except Exception as e:
+                    self.log.error(f'Bad template file or template not defined in {jobfile}: {e}')
+                    os.remove(jobfile)
+                    continue
+
+                try:
+                    for jobconfig in self.generateJobs(config):
+                        newjobfile = os.path.join(self.tmpdir, os.path.basename(jobfile))
+                        with tempfile.NamedTemporaryFile(mode='w', prefix=f'{newjobfile}.', delete=False, encoding='utf-8') as f:
+                            newjobfile = f.name
+                            f.write('\n'.join(f'{k}={v}' for k,v in jobconfig.items()))
+
+                        self.dbldmx.insertJob(newjobfile, templatefile, proxyid)
+                        self.log.info(f'Inserted job from {newjobfile} into DB')
+                except Exception as e:
+                    raise
+                    self.log.error(f'Failed to create jobs from {jobfile}: {e}')
+            os.remove(jobfile)
 
 
     def process(self):
