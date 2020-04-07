fcc610538f5740c6a606ed9228b20b0242570b5a
David Cameron <d.g.cameron@gmail.com>
2020-04-07 21:55:55 +0200

    allow duplicate keys in xrsl

--- src/act/ldmx/aCTLDMX2Arc.py
+++ src/act/ldmx/aCTLDMX2Arc.py
@@ -59,7 +59,7 @@ class aCTLDMX2Arc(aCTLDMXProcess):
             arcid = self.dbarc.insertArcJobDescription(xrsl,
                                                        proxyid=job['proxyid'],
                                                        clusterlist=','.join(onlineendpoints),
-                                                       downloadfiles='stdout;gmlog/errors;rucio.metadata',
+                                                       downloadfiles='gmlog/errors;stdout;rucio.metadata',
                                                        appjobid=str(job['id']))
 
             desc = {'ldmxstatus': 'submitted', 'arcjobid': arcid['LAST_INSERT_ID()']}
@@ -81,26 +81,29 @@ class aCTLDMX2Arc(aCTLDMXProcess):
         # Parse some requirements from descriptionfile
         with open(descriptionfile) as f:
             config = {l.split('=')[0]: l.split('=')[1].strip() for l in f}
-            xrsl['memory'] = int(config.get('JobMemory', 2)) * 1000
-            xrsl['walltime'] = int(config.get('JobWallTime', 240))
-            xrsl['cputime'] = xrsl['walltime']
+            xrsl['memory'] = f"(memory = {int(config.get('JobMemory', 2)) * 1000})"
+            xrsl['walltime'] = f"(walltime = {int(config.get('JobWallTime', 240))})"
+            xrsl['cputime'] = f"(cputime = {int(config.get('JobWallTime', 240))})"
+            xrsl['defaultrte'] = "(runtimeenvironment = APPS/LDMX-SIMPROD-1.0)"
             if 'RunTimeEnvironment' in config: # TODO multiple RTE!
-                xrsl['runtimeenvironment'] = f"APPS/{config.get('RunTimeEnvironment')}"
+                xrsl['runtimeenvironment'] = f"(runtimeenvironment = APPS/{config.get('RunTimeEnvironment')})"
 
         wrapper = self.conf.get(['executable', 'wrapper'])
-        xrsl['executable'] = os.path.basename(wrapper)
-        xrsl['inputfiles'] = f'({os.path.basename(wrapper)} {wrapper}) \
-                               (ldmxjob.config {descriptionfile}) \
-                               (ldmx.mac.template {templatefile}) \
-                               (ldmx-prod-helper.py {self.conf.get(["executable", "ruciohelper"])})'
-        xrsl['stdout'] = 'stdout'
-        xrsl['gmlog'] = 'gmlog'
-        xrsl['join'] = 'yes'
-        xrsl['rerun'] = '2'
-        xrsl['outputfiles'] = '("rucio.metadata" "")'
-        xrsl['jobName'] = '"LDMX Prod Simulation"'
+        xrsl['executable'] = f"(executable = {os.path.basename(wrapper)})"
+        xrsl['inputfiles'] = f'(inputfiles = ({os.path.basename(wrapper)} {wrapper}) \
+                                             (ldmxjob.config {descriptionfile}) \
+                                             (ldmxsim.mac.template {templatefile}) \
+                                             (ldmx-prod-helper.py {self.conf.get(["executable", "ruciohelper"])}))'
+        xrsl['stdout'] = '(stdout = stdout)'
+        xrsl['gmlog'] = '(gmlog = gmlog)'
+        xrsl['join'] = '(join = yes)'
+        xrsl['rerun'] = '(rerun = 2)'
+        xrsl['count'] = '(count = 1)'
+        xrsl['outputfiles'] = '(outputfiles = ("rucio.metadata" ""))'
+        xrsl['jobName'] = '(jobname = "LDMX Prod Simulation")'
+        self.log.info(xrsl)
 
-        return '&' + '\n'.join(f'({k} = {v})' for k,v in xrsl.items())
+        return '&' + '\n'.join(xrsl.values())
 
 
     def process(self):
