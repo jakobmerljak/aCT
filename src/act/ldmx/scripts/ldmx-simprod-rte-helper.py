19217cd31a5d0e3f6bf3427025dd954fd8e3538e
David Cameron <d.g.cameron@gmail.com>
2020-07-06 22:20:16 +0200

    add pre-job processing

--- src/act/ldmx/scripts/ldmx-simprod-rte-helper.py
+++ src/act/ldmx/scripts/ldmx-simprod-rte-helper.py
@@ -243,7 +243,7 @@ def get_parser():
                         help='LDMX Production simulation parameter dump JSON file')
     parser.add_argument('-j', '--json-metadata', action='store', default='rucio.metadata',
                         help='LDMX Production simulation JSON metadata file')
-    parser.add_argument('action', choices=['collect-metadata', 'test'],
+    parser.add_argument('action', choices=['init', 'collect-metadata', 'test'],
                         help='Helper action to perform')
     return parser
 
@@ -257,12 +257,18 @@ if __name__ == '__main__':
     # config is parsed for any action
     conf_dict = parse_ldmx_config(cmd_args.config)
 
-    # template substitution (RTE stage 1)
     if cmd_args.action == 'test' :
         collect_from_json( "metadata_ldmx_v12_ecal_pn_run0.json" )
+    elif cmd_args.action == 'init':
+        # store job start time
+        job_starttime()
+        # print values for bash eval
+        print_eval(conf_dict)
     elif cmd_args.action == 'collect-metadata':
         meta = collect_meta(conf_dict, cmd_args.metaDump)
-        print(meta.get('DataLocation', ''))
+        if 'DataLocation' not in meta:
+            sys.exit(1)
+        print('export FINALOUTPUTFILE={DataLocation}'.format(**meta))
         with open(cmd_args.json_metadata, 'w') as meta_f:
             json.dump(meta, meta_f)
 
