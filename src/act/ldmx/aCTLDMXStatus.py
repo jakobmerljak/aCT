c68ad3e3e43672e99298c58fe5f5dc685c0d57c7
David Cameron <d.g.cameron@gmail.com>
2020-09-10 08:13:11 +0000

    handle cancelling jobs with no arc id

--- src/act/ldmx/aCTLDMXStatus.py
+++ src/act/ldmx/aCTLDMXStatus.py
@@ -81,7 +81,7 @@ class aCTLDMXStatus(aCTLDMXProcess):
 
             # Check if there is an arc job
             columns = ['id', 'JobID']
-            arcjob = self.dbarc.getArcJobInfo(job['arcjobid'], columns)
+            arcjob = self.dbarc.getArcJobInfo(job['arcjobid'], columns) if job['arcjobid'] else None
             if arcjob:
                 self.log.info(f"Cancelling arc job {arcjob['id']}")
                 select = "id='{}'".format(job['arcjobid'])
